6b728a16aea33733a885729de27ec6581611da11
heesukim628 <heesukim628@gmail.com>
2025-12-25 07:24:42 -0500

    change the layout of tag and date

--- layouts/ListLayoutWithTags.tsx
+++ layouts/ListLayoutWithTags.tsx
@@ -84,7 +84,6 @@ export default function ListLayoutWithTags({
   const pathname = usePathname()
   const tagCounts = tagData as Record<string, number>
   const tagKeys = Object.keys(tagCounts)
-  const sortedTags = tagKeys.sort((a, b) => tagCounts[b] - tagCounts[a])
 
   const displayPosts = initialDisplayPosts.length > 0 ? initialDisplayPosts : posts
 
@@ -131,7 +130,6 @@ export default function ListLayoutWithTags({
                     <ul className="ml-2 space-y-1 border-l-2 border-gray-200 pl-3 dark:border-gray-700">
                       {tags
                         .filter((tag) => tagCounts[tag] > 0)
-                        .sort((a, b) => tagCounts[b] - tagCounts[a])
                         .map((tag) => (
                           <li key={tag}>
                             <Link
@@ -204,24 +202,24 @@ export default function ListLayoutWithTags({
                 return (
                   <li key={path} className="py-5">
                     <article className="flex flex-col space-y-2 xl:space-y-0">
-                      <dl>
-                        <dt className="sr-only">Published on</dt>
-                        <dd className="text-base leading-6 font-medium text-gray-500 dark:text-gray-400">
-                          <time dateTime={date} suppressHydrationWarning>
-                            {formatDate(date, siteMetadata.locale)}
-                          </time>
-                        </dd>
-                      </dl>
                       <div className="space-y-3">
                         <div>
-                          <h2 className="text-2xl leading-8 font-bold tracking-tight">
-                            <Link href={`/${path}`} className="text-gray-900 dark:text-gray-100">
-                              {title}
-                            </Link>
-                          </h2>
-                          <div className="flex flex-wrap">
+                          <div className="flex flex-wrap items-end">
+                            <h2 className="mr-4 text-2xl leading-8 font-bold tracking-tight">
+                              <Link href={`/${path}`} className="text-gray-900 dark:text-gray-100">
+                                {title}
+                              </Link>
+                            </h2>
                             {tags?.map((tag) => <Tag key={tag} text={tag} />)}
                           </div>
+                          <dl>
+                            <dt className="sr-only">Published on</dt>
+                            <dd className="text-base leading-6 font-medium text-gray-500 dark:text-gray-400">
+                              <time dateTime={date} suppressHydrationWarning>
+                                {formatDate(date, siteMetadata.locale)}
+                              </time>
+                            </dd>
+                          </dl>
                         </div>
                         <div className="prose max-w-none text-gray-500 dark:text-gray-400">
                           {summary}
