c9e880858857fd098dbbc3f5dcd9db915017e082
heesukim628 <heesukim628@gmail.com>
2025-12-25 05:44:07 -0500

    remove seo

--- app/tags/[tag]/page.tsx
+++ app/tags/[tag]/page.tsx
@@ -4,27 +4,27 @@ import siteMetadata from '@/data/siteMetadata'
 import ListLayout from '@/layouts/ListLayoutWithTags'
 import { allBlogs } from 'contentlayer/generated'
 import tagData from 'app/tag-data.json'
-import { genPageMetadata } from 'app/seo'
+// import { genPageMetadata } from 'app/seo'
 import { Metadata } from 'next'
 
 const POSTS_PER_PAGE = 5
 
-export async function generateMetadata(props: {
-  params: Promise<{ tag: string }>
-}): Promise<Metadata> {
-  const params = await props.params
-  const tag = decodeURI(params.tag)
-  return genPageMetadata({
-    title: tag,
-    description: `${siteMetadata.title} ${tag} tagged content`,
-    alternates: {
-      canonical: './',
-      types: {
-        'application/rss+xml': `${siteMetadata.siteUrl}/tags/${tag}/feed.xml`,
-      },
-    },
-  })
-}
+// export async function generateMetadata(props: {
+//   params: Promise<{ tag: string }>
+// }): Promise<Metadata> {
+//   const params = await props.params
+//   const tag = decodeURI(params.tag)
+//   return genPageMetadata({
+//     title: tag,
+//     description: `${siteMetadata.title} ${tag} tagged content`,
+//     alternates: {
+//       canonical: './',
+//       types: {
+//         'application/rss+xml': `${siteMetadata.siteUrl}/tags/${tag}/feed.xml`,
+//       },
+//     },
+//   })
+// }
 
 export const generateStaticParams = async () => {
   const tagCounts = tagData as Record<string, number>
